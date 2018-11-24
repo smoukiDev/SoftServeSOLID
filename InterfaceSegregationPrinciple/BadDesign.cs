75f0da15b5060641979bd82ed204ddce2a810183
smoukiDev <smakfactory@gmail.com>
2018-11-24 23:55:05 +0200

    Interface segregation princriple. Added bad design example and good design example.

---
+++ InterfaceSegregationPrinciple/BadDesign.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace InterfaceSegregationPrinciple.BadDesign
+{
+    /// <summary>
+    /// Fat interface
+    /// </summary>
+    public interface IMessage
+    {
+        void Send();
+        string Sender { get; set; }
+        string Receiver { get; set; }
+        string Subject { get; set; }
+        string Text { get; set; }
+        byte[] Voice { get; set; }
+    }
+
+    public class EmailMessage : IMessage
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Text { get; set; }
+        /// <summary>
+        /// Isn't used
+        /// </summary>
+        public byte[] Voice
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public void Send()
+        {
+            Console.WriteLine($"Send letter from {this.Sender} to {this.Receiver} ");
+            Console.WriteLine($"Send {this.Subject}");
+            Console.WriteLine($"Send {this.Text}");
+        }
+    }
+
+    public class SMSMessage : IMessage
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Text { get; set; }
+        /// <summary>
+        /// Isn't used
+        /// </summary>
+        public string Subject
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Isn't used
+        /// </summary>
+        public byte[] Voice
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public void Send()
+        {
+            Console.WriteLine($"Send SMS-message from {this.Sender} to {this.Receiver} ");
+            Console.WriteLine($"Send {this.Text}");
+        }
+    }
+
+    public class VoiceMessage : IMessage
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        /// <summary>
+        /// Isn't used
+        /// </summary>
+        public string Subject
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Isn't used
+        /// </summary>
+        public string Text
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public byte[] Voice { get; set; }
+        public void Send()
+        {
+            Console.WriteLine($"Send Voice-message from {this.Sender} to {this.Receiver} ");
+            Console.WriteLine($"Send {nameof(this.Voice)}");
+        }
+    }
+}
---
+++ InterfaceSegregationPrinciple/GoodDesign.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace InterfaceSegregationPrinciple.GoodDesign
+{
+    /// <summary>
+    /// Common interface
+    /// </summary>
+    public interface IMessage
+    {
+        void Send();
+        string Sender { get; set; }
+        string Receiver { get; set; }
+    }
+
+    /// <summary>
+    /// Interface extends IMessgage interface
+    /// </summary>
+    public interface ITextMessage : IMessage
+    {
+        string Text { get; set; }
+    }
+
+    /// <summary>
+    /// Interface extends ITextMessage interface
+    /// </summary>
+    public interface IEmailMessage : ITextMessage
+    {
+        string Subject { get; set; }
+    }
+
+    /// <summary>
+    /// Interface extends IMessage interface
+    /// </summary>
+    public interface IVoiceMessage : IMessage
+    {
+        byte[] Voice { get; set; }
+    }
+
+    /// <summary>
+    /// Class implements methods of IEmailMessage interface.
+    /// Interface provides only required method for concrete purpose (SRP)
+    /// Class doesn't force to implement needless functionality or mock useless method.
+    /// </summary>
+    public class EmailMessage : IEmailMessage
+    {
+        public string Subject { get; set; }
+        public string Text { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public void Send()
+        {
+            Console.WriteLine($"Send letter from {this.Sender} to {this.Receiver} ");
+            Console.WriteLine($"Send {this.Subject}");
+            Console.WriteLine($"Send {this.Text}");
+        }
+    }
+
+    /// <summary>
+    /// Class implements methods of ITextMessage interface.
+    /// Interface provides only required method for concrete purpose (SRP)
+    /// Class doesn't force to implement needless functionality or mock useless method.
+    /// </summary>
+    public class SMSMessage : ITextMessage
+    {
+        public string Text { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public void Send()
+        {
+            Console.WriteLine($"Send SMS-message from {this.Sender} to {this.Receiver} ");
+            Console.WriteLine($"Send {this.Text}");
+        }
+    }
+
+    /// <summary>
+    /// Class implements methods of IVoiceMessage interface.
+    /// Interface provides only required method for concrete purpose (SRP)
+    /// Class doesn't force to implement needless functionality or mock useless method.
+    /// </summary>
+    public class VoiceMessage : IVoiceMessage
+    {
+        public byte[] Voice { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public void Send()
+        {
+            Console.WriteLine($"Send Voice-message from {this.Sender} to {this.Receiver} ");
+            Console.WriteLine($"Send {nameof(this.Voice)}");
+        }
+    }
+}
